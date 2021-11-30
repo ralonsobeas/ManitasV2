f414412546df8468d6619015eda271179f4ebcf4
gennakk <44756216+gennakk@users.noreply.github.com>
2021-11-30 11:04:10 +0100

    Commit3

--- manos.js
+++ manos.js
@@ -6,7 +6,7 @@ function clickMano(id){
 	  if(bloqueado){
       	return;
     }
-  
+
     puntosJugador = parseInt($("#pJugador").text());
     puntosMaquina = parseInt($("#pMaquina").text());
 
@@ -23,35 +23,35 @@ function clickMano(id){
 
     //random
     var numero = Math.round(Math.random()+1);
-  
+
     $("#llavePos").text(numero);
 }
 
 function encontrarLlave(id){
   	bloqueado = true;
-  
+
   	$("#cerrada"+id).toggle();
     setTimeout(function () {$("#cerrada"+id2).toggle();}, 1000);
-  
+
   	var id2 = (id == '1') ? 2 : 1;
-  
+
   	var acierto = ($("#llavePos").text()==id);
-  
+
   	if(acierto){
     		$("#llave"+id).toggle();
       	setTimeout(function () {$("#abierta"+id2).toggle();}, 1000);
-      
+
     		setTimeout(function () {$("#llave"+id).toggle();}, 2000);
       	setTimeout(function () {$("#abierta"+id2).toggle();}, 2000);
     }
   	else{
     		$("#abierta"+id).toggle();
       	setTimeout(function () {$("#llave"+id2).toggle();}, 1000);
-      
+
     		setTimeout(function () {$("#abierta"+id).toggle();}, 2000);
       	setTimeout(function () {$("#llave"+id2).toggle();}, 2000);
     }
     setTimeout(function () {$("#cerrada"+id).toggle(); $("#cerrada"+id2).toggle(); bloqueado = false;}, 2000);
-  
+
     return acierto;
 }
